8201c765f2db800a5117584c1363a8b6f12a00d5
Andres <aparrag@cifpfbmoll.eu>
2021-04-20 16:20:05 +0200

    Añadida clase Administrativo
    creada clase administrativo con constructor de superclase

---
+++ MCE/src/ObjetosCrucero/Servicios/Administrativo.java
@@ -0,0 +1,12 @@
+package ObjetosCrucero.Servicios;
+
+public class Administrativo extends Usuario{
+    //private int nivelAcceso;???
+    //private String seccion;???
+
+    public Administrativo(String codigoEmpleado, String nieEmpleado, String nombreEmpleado, String apellidoEmpleado) {
+        super(codigoEmpleado, nieEmpleado, nombreEmpleado, apellidoEmpleado);
+    }
+
+    //falta añadir getters, setters, toString y todos los metodos necesarios
+}

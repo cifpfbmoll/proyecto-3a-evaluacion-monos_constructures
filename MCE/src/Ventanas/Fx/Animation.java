fc920095dbb056e54dc8d69bb2b0127a22417112
Jose Luis Saiz <jsaiz@cifpfbmoll.eu>
2021-04-20 16:55:38 +0200

    Añadida la fecha y hora al menu
    He añadido la fecha y hora dentro del menu del usuario creando un nuevo "thread" en la clase de animaciones.

--- MCE/src/Ventanas/Fx/Animation.java
+++ MCE/src/Ventanas/Fx/Animation.java
@@ -1,10 +1,17 @@
 package Ventanas.Fx;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.ScaleTransition;
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.util.Duration;
+
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.TextStyle;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Animation {
 
@@ -50,14 +57,39 @@ public class Animation {
 	}
 
 
-	//Paquete de animaciones de botones
 
-	public static void button_animation_GROW(Node node){
-		ScaleTransition scale = new ScaleTransition(Duration.millis(2000), node);
-		scale.setByX(1.5f);
-		scale.setByY(1.5f);
-		scale.setAutoReverse(false);
-		scale.play();
+
+	//Actualizar fecha y hora en un nodo (Posiblemente un label)
+
+	public static void setFechaYHora (Label label) {
+		Timeline timeline = new Timeline(
+				new KeyFrame(Duration.seconds(0),
+						actionEvent -> {
+
+							// Obtenemos la fecha
+							Month mes = LocalDate.now().getMonth();
+							String mesString = mes.getDisplayName(TextStyle.FULL, new Locale("es", "ES"));
+
+							DayOfWeek dia = LocalDate.now().getDayOfWeek();
+							String diaString = dia.getDisplayName(TextStyle.FULL, new Locale("es", "ES"));
+
+							String diaStringFormat = diaString.substring(0, 1).toUpperCase() + diaString.substring(1);
+
+							String diaMes = Integer.toString(MonthDay.now().getDayOfMonth());
+
+							String anyoString = Integer.toString(LocalDate.now().getYear());
+
+							// Obtenemos la hora
+							Calendar time = Calendar.getInstance();
+							SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+							String hora = simpleDateFormat.format(time.getTime());
+
+							label.setText(diaStringFormat + " " + diaMes + " de " + mesString + " del " + anyoString + ", " + hora);
+						}
+				),
+				new KeyFrame(Duration.seconds(1))
+		);
+		timeline.setCycleCount(javafx.animation.Animation.INDEFINITE);
+		timeline.play();
 	}
-
 }

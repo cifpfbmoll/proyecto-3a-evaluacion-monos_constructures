8a413c8a860f97c5294743e37ad120829d4cba6f
Jose Luis Saiz <jsaiz@cifpfbmoll.eu>
2021-04-21 19:10:40 +0200

    Creada la clase Usuario y Empleado
    Se han modificado las clases para una mayor compresión y utilización.

--- MCE/src/ObjetosCrucero/Servicios/Usuario.java
+++ MCE/src/ObjetosCrucero/Servicios/Usuario.java
@@ -2,64 +2,41 @@ package ObjetosCrucero.Servicios;
 
 public abstract class Usuario {
 
-	private String codigoEmpleado;
-	private String nieEmpleado;
-	private String nombreEmpleado;
-	private String apellidoEmpleado;
-	private String nombreServicio;
+	private String nombre;
+	private String apellidos;
+	private String nie;
+
+	public Usuario(String nombre, String apellidos, String nie){
+		this.setNombre(nombre);
+		this.setApellidos(apellidos);
+		this.setNie(nie);
+	}
+
 
 	//Getters y setters
 
-	public String getCodigoEmpleado() {
-		return codigoEmpleado;
+	public String getNombre() {
+		return nombre;
 	}
 
-	public void setCodigoEmpleado(String codigoEmpleado) {
-		this.codigoEmpleado = codigoEmpleado;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
-	public String getNieEmpleado() {
-		return nieEmpleado;
+	public String getApellidos() {
+		return apellidos;
 	}
 
-	public void setNieEmpleado(String nieEmpleado) {
-		this.nieEmpleado = nieEmpleado;
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
 	}
 
-	public String getNombreEmpleado() {
-		return nombreEmpleado;
+	public String getNie() {
+		return nie;
 	}
 
-	public void setNombreEmpleado(String nombreEmpleado) {
-		this.nombreEmpleado = nombreEmpleado;
+	public void setNie(String nie) {
+		this.nie = nie;
 	}
-
-	public String getApellidoEmpleado() {
-		return apellidoEmpleado;
-	}
-
-	public void setApellidoEmpleado(String apellidoEmpleado) {
-		this.apellidoEmpleado = apellidoEmpleado;
-	}
-
-	public String getNombreServicio() {
-		return nombreServicio;
-	}
-
-	public void setNombreServicio(String nombreServicio) {
-		this.nombreServicio = nombreServicio;
-	}
-
-	/**
-	 * Constructor con todos los parámetros
-	 */
-	public Usuario(String codigoEmpleado, String nieEmpleado, String nombreEmpleado, String apellidoEmpleado, String nombreServicio) {
-		this.setCodigoEmpleado(codigoEmpleado);
-		this.setNieEmpleado(nieEmpleado);
-		this.setNombreEmpleado(nombreEmpleado);
-		this.setApellidoEmpleado(apellidoEmpleado);
-		this.setNombreServicio(nombreServicio);
-	}
-
 
 }

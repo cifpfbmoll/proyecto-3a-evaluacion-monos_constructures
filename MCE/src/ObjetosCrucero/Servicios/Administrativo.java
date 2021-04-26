844fb13ed1ce6cbf4577e0c49c0fc8bfd9b3b70a
Andres <aparrag@cifpfbmoll.eu>
2021-04-26 19:42:20 +0200

    Preparado para añadir metodos
    Arreglado error en el constructor y añadidos metodos comentados

--- MCE/src/ObjetosCrucero/Servicios/Administrativo.java
+++ MCE/src/ObjetosCrucero/Servicios/Administrativo.java
@@ -1,12 +1,22 @@
 package ObjetosCrucero.Servicios;
 
 public class Administrativo extends Empleado{
-    //private int nivelAcceso;???
-    //private String seccion;???
 
-    public Administrativo(String codigoEmpleado, String nieEmpleado, String nombreEmpleado, String apellidoEmpleado) {
-        super(codigoEmpleado, nieEmpleado, nombreEmpleado, apellidoEmpleado);
+    /**
+     * Constructor con parametros de superclase
+     */
+    public Administrativo(String codigoEmpleado, String nieEmpleado, String nombreEmpleado, String apellidoEmpleado, String nombreServicio) {
+        super(codigoEmpleado, nieEmpleado, nombreEmpleado, apellidoEmpleado, nombreServicio);
     }
 
-    //falta añadir getters, setters, toString y todos los metodos necesarios
+    /*Metodos de clase
+    La clase Administrativo tiene acceso al estado del crucero, a la lista de pasajeros y a la lista de la tipulación
+    */
+
+    //public static* ArrayList<Crucero> getEstadoCrucero();
+
+    //public static* ArrayList<Cliente> getListaPasajeros();
+
+    //public static* ArrayList<Empleado> getListaTripulacion();
+
 }

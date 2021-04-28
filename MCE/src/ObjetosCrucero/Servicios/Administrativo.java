88316c1fdd7121efc4f8e0a66eca8eb2938f94bb
Andres <aparrag@cifpfbmoll.eu>
2021-04-28 19:12:11 +0200

    metodo getEstadoCrucero
    agregado metodo getEstadoCrucero que recupera de la base de datos las especificaciones de todos los cruceros, los recoge en un arrayList y los devuelve

--- MCE/src/ObjetosCrucero/Servicios/Administrativo.java
+++ MCE/src/ObjetosCrucero/Servicios/Administrativo.java
@@ -1,5 +1,12 @@
 package ObjetosCrucero.Servicios;
 
+import Utils.DBUtils;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Administrativo extends Empleado{
 
     /**
@@ -13,10 +20,34 @@ public class Administrativo extends Empleado{
     La clase Administrativo tiene acceso al estado del crucero, a la lista de pasajeros y a la lista de la tipulación
     */
 
-    //public static* ArrayList<Crucero> getEstadoCrucero();
+    public static ArrayList<ObjetosCrucero.Crucero> getEstadoCrucero() throws SQLException {
+        ArrayList<ObjetosCrucero.Crucero> listaCruceros = new ArrayList();
 
-    //public static* ArrayList<Cliente> getListaPasajeros();
+        String crucerosSQL = "SELECT * FROM CRUCERO";
+        PreparedStatement sentencia = DBUtils.getConnectionDB().prepareStatement(crucerosSQL);
+        ResultSet resultSet = sentencia.executeQuery();
 
-    //public static* ArrayList<Empleado> getListaTripulacion();
+        ObjetosCrucero.Crucero crucero;
+
+        while (resultSet.next()) {
+            crucero = new ObjetosCrucero.Crucero();
+            crucero.setCodigoCrucero(resultSet.getString("CODIGO_CRUCERO"));
+            crucero.setNombreCrucero(resultSet.getString("NOMBRE_CRUCERO"));
+            crucero.setModeloCrucero(resultSet.getString("MODELO_CRUCERO"));
+            crucero.setEslora(resultSet.getInt("ESLORA"));
+            crucero.setManga(resultSet.getInt("MANGA"));
+            crucero.setCalado(resultSet.getInt("CALADO"));
+
+            listaCruceros.add(crucero);
+        }
+        DBUtils.getConnectionDB().close();
+        return listaCruceros;
+    }
+
+    //public static ArrayList<Cliente> getListaPasajeros();
+
+    public static ArrayList<Empleado> getListaTripulacion() {
+        return null;
+    }
 
 }

ed8cd172d4d9a30cf27b5cb767d76b683a7c01bb
David <dsastref@cifpfbmoll.eu>
2021-04-28 20:10:07 +0200

    Creacion Clase Billete
    Se crea la clase billete con Atributos, getters & setters, con la inestimable ayude de Roberto.

--- MCE/src/ObjetosCrucero/Servicios/Billete.java
+++ MCE/src/ObjetosCrucero/Servicios/Billete.java
@@ -1,4 +1,50 @@
 package ObjetosCrucero.Servicios;
+import java.util.Date;
 
 public class Billete {
+    //Atributos
+    
+    private String nie;
+    private Date fechaEmbarque;
+    private String codigoCamarote;
+
+    //Getter&Setter
+
+    public String getNie() {
+        return nie;
+    }
+
+    public void setNie(String nie) {
+        this.nie = nie;
+    }
+
+    public Date getFechaEmbarque() {
+        return fechaEmbarque;
+    }
+
+    public void setFechaEmbarque(Date fechaEmbarque) {
+        this.fechaEmbarque = fechaEmbarque;
+    }
+
+    public String getCodigoCamarote() {
+        return codigoCamarote;
+    }
+
+    public void setCodigoCamarote(String codigoCamarote) {
+        this.codigoCamarote = codigoCamarote;
+    }
+
+    //Construsctor
+
+    public Billete(String nie, Date fechaEmbarque, String codigoCamarote) {
+        this.setNie(nie);
+        this.setFechaEmbarque(fechaEmbarque);
+        this.setCodigoCamarote(codigoCamarote);
+    }
+    /*//Métodos
+    String sqlNuevoBillete = "INSERT INTO Billete VALUES (?,?,?)";
+    */
+
 }
+
+

696e8d5a018e61528da8d63b9a8ea2a2ee931447
Andres <aparrag@cifpfbmoll.eu>
2021-04-28 20:18:21 +0200

    metodo getTripulacion
    metodo que devuelve un arrayList con la tripulacion presente en un crucero y en un viaje en concreto

--- MCE/src/ObjetosCrucero/Servicios/Administrativo.java
+++ MCE/src/ObjetosCrucero/Servicios/Administrativo.java
@@ -2,6 +2,7 @@ package ObjetosCrucero.Servicios;
 
 import Utils.DBUtils;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -23,7 +24,8 @@ public class Administrativo extends Empleado{
     public static ArrayList<ObjetosCrucero.Crucero> getEstadoCrucero() throws SQLException {
         ArrayList<ObjetosCrucero.Crucero> listaCruceros = new ArrayList();
 
-        String crucerosSQL = "SELECT * FROM CRUCERO";
+        DBUtils.createConnectionDB();
+        String crucerosSQL = "SELECT * FROM CRUCERO;";
         PreparedStatement sentencia = DBUtils.getConnectionDB().prepareStatement(crucerosSQL);
         ResultSet resultSet = sentencia.executeQuery();
 
@@ -44,10 +46,36 @@ public class Administrativo extends Empleado{
         return listaCruceros;
     }
 
-    //public static ArrayList<Cliente> getListaPasajeros();
-
-    public static ArrayList<Empleado> getListaTripulacion() {
+    public static ArrayList<Cliente> getListaPasajeros() {
         return null;
+    }
+
+    public static ArrayList<Empleado> getListaTripulacion() throws SQLException {
+        ArrayList<Empleado> listaTripulacion = new ArrayList();
+
+        DBUtils.createConnectionDB();
+        String tripulacionSQL = "SELECT * FROM empleado, viaje, asignacion " +
+                "WHERE empleado.CODIGO_EMPLEADO = asignacion.CODIGO_EMPLEADO " +
+                "and asignacion.CODIGO_EMPLEADO LIKE ?;";
+        PreparedStatement sentencia = DBUtils.getConnectionDB().prepareStatement(tripulacionSQL);
+        ResultSet resultSet = sentencia.executeQuery();
+
+        Empleado tripulacion;
+
+        while (resultSet.next()) {
+            tripulacion = new Empleado(
+                    resultSet.getString("CODIGO_EMPLEADO"),
+                    resultSet.getString("NIE_EMPLEADO"),
+                    resultSet.getString("NOMBRE_EMPLEADO"),
+                    resultSet.getString("APELLIDO_EMPLEADO"),
+                    TipoServicio.valueOf(resultSet.getString("CODIGO_SERVICIO")),
+                    resultSet.getString("DOMICILIACION_EMPLEADO"),
+                    Date.valueOf(resultSet.getString("FECHA_NACIMIENTO_EMPLEADO"))
+            );
+            listaTripulacion.add(tripulacion);
+        }
+        DBUtils.getConnectionDB().close();
+        return listaTripulacion;
     }
 
 }
